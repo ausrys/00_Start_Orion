03f916c384f6ebf8331939b6af67d157d9344356
Ausrys Work <ausrys.mikoliunas@orioninc.com>
2025-04-08 11:50:57 +0300

    edited face code

--- Sprint_01/rate_facade.py
+++ Sprint_01/rate_facade.py
@@ -11,20 +11,31 @@ class ExchangeRateService:
             "rate": 1.1 * eur,
             "timestamp": "2025-04-07T10:00:00"
         }
-        eur_to_pound = {
-            "currency": "GBP",
-            "rate": 0.85 * eur,
-            "timestamp": "2025-04-07T10:00:00"
-        }
-        eur_to_yen = {
-            "currency": "YEN",
-            "rate": 160 * eur,
-            "timestamp": "2025-04-07T10:00:00"
-        }
-        return eur_to_usd, eur_to_pound, eur_to_yen
+        return eur_to_usd
 
 
-# The Facade class
+class ExchangeServiceCost:
+    """Cost for exchange servise."""
+
+    def get_discount(self, eur):
+        # Simulating a response from an external exchange rate API
+        eur_to_usd_cost = {
+            "current": eur,
+            "cost": 0.10,
+            "final": eur + 0.10
+        }
+        return eur_to_usd_cost
+
+
+class ExchangeLimit:
+    """Limit for exchange servise."""
+
+    def get_limit(self):
+        # Simulating a response from an external exchange rate API
+        eur_to_usd_limit = {
+            "limit": 100000
+        }
+        return eur_to_usd_limit
 
 
 class RateFacade:
@@ -32,11 +43,18 @@ class RateFacade:
 
     def __init__(self):
         self.exchange_rate_service = ExchangeRateService()
+        self.exchange_rate_cost = ExchangeServiceCost()
+        self.exchange_limit = ExchangeLimit()
 
-    def get_exchange_rate(self, number):
+    def get_rates(self, number):
         # Get exchange rate information from the service
-        rate_data = self.exchange_rate_service.get_exchange_rate(number)
-        return json.dumps(rate_data, indent=4)
+        all_rates = {
+            "rate_exchange_cost": self.exchange_rate_cost.get_discount(number),
+            "rate_exchange": self.exchange_rate_service.get_exchange_rate(
+                number),
+            "exchange_limit": self.exchange_limit.get_limit()
+        }
+        return json.dumps(all_rates, indent=4)
 
 # Client code
 
@@ -45,8 +63,8 @@ def main():
     facade = RateFacade()
 
     # Simulate getting exchange rate data
-    print("Exchange Rate JSON:")
-    print(facade.get_exchange_rate(100))
+    print("Exchange Rates EUR TO :")
+    print(facade.get_rates(100))
 
 
 if __name__ == "__main__":
